1a524369218842feb4e6e9a57c0b8423af7bcbf3
Brendan Magee <bmagee@gmail.com>
2016-04-19 10:26:28 -0400

    began setting up angular app

---
+++ js/app.js
@@ -0,0 +1,15 @@
+(function(){
+  "use strict";
+
+  angular
+  .module("portfolio", [])
+  .controller("PortfolioController", PortfolioController);
+
+  /* PortfolioController function */
+  function PortfolioController () {
+    var vm = this;
+  }
+
+
+
+})();
