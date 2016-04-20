26dc94758fe1cf66e0883963f38640781340cdad
Brendan Magee <bmagee@gmail.com>
2016-04-20 10:44:14 -0400

    back to the drawing board

--- js/app.js
+++ js/app.js
@@ -2,29 +2,11 @@
   "use strict";
 
   angular
-  .module("portfolio", [
-    "ui.router",
-    "projects"
-  ])
-  .config([
-    "$stateProvider",
-    Router
-  ]);
+  .module("portfolio", [])
+  .controller("PortfolioController", PortfolioController);
 
-  function Router($stateProvider){
-    $stateProvider
-    .state("projectIndex", {
-      url: "/projects",
-      templateUrl: "js/projects/index.html",
-      controller: "IndexController",
-      controllerAs: "IndexVM"
-    })
-    .state("projectShow", {
-      url: "/projects/:title",
-      templateUrl: "js/projects/show.html",
-      controller: "ShowController",
-      controllerAs: "ShowVM"
-    });
+  function PortfolioController () {
+    var vm = this;
   }
 
 })();
